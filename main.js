5fb70e589ccfa0ea61dfe078777abcc9f195f1d2
huuphuoc2400 <phuocbe123@gmail.com>
2024-04-23 21:09:06 +0700

    add responsive for website

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
+const yourDate = new Date("2023-07-18T00:00:00")
+document.addEventListener('DOMContentLoaded', function () {
+    const today = new Date()
+    setInterval(function () {
+        document.querySelector("date").textContent = Math.floor(Math.floor((today - yourDate) / 1000) / 60 / 60 / 24) + " DAYS"
+    }, 1000)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
