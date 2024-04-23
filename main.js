ec81a8c7c33ea15b116c3227029abd85936ecebc
huuphuoc2400 <phuocbe123@gmail.com>
2024-04-23 22:31:40 +0700

    add responsive for website

--- main.js
+++ main.js
@@ -1,34 +1,22 @@
-const yourDate = new Date("2023-07-18T00:00:00")
-document.addEventListener('DOMContentLoaded', function () {
-    const today = new Date()
-    setInterval(function () {
-        document.querySelector("date").textContent = Math.floor(Math.floor((today - yourDate) / 1000) / 60 / 60 / 24) + " DAYS"
-    }, 1000)
-})
+const firstDate = new Date("2023-07-18T00:00:00");
 
+function formatNumber(num) {
+  return num < 10 ? `0${num}` : num;
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+  setInterval(function () {
+    const date = new Date();
+    const diff = date.getTime() - firstDate.getTime();
 
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    document.getElementById("date").textContent =
+    `${days} DAYS`
+    document.getElementById("time").textContent =
+      `${hours}:${minutes}:${formatNumber(seconds)}`;
+  }, 1000);
+});
